0f0415571ee7d2de189b35744dfb9aaceff7c9fa
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2022-12-26 17:27:05 -0500

    added models for refreshing token

---
+++ StravaAPIModels/Models/Authorization/ShortLivedAccessToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StravaAPIModels.Library.Models.Authorization
+{
+    public class ShortLivedAccessToken
+    {
+        public int AthleteId { get; set; }
+        public bool Scope { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
